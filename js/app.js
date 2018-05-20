d75e2e2e0df4d03568ebc73a2bf20412e4bec43b
riccio85 <rihanna.ke@gmail.com>
2018-05-20 23:17:58 +0200

    comments added

--- js/app.js
+++ js/app.js
@@ -11,8 +11,19 @@
 
 const enemyXvalues = [-50,-100, -150];
 const enemyYvalues = [50, 130, 210];
+const totalEnemies = 3;
+let allEnemies = [];
+let player = null;
+let gameWinned = false; //used for the alert when the game is winned
 
+/**
+ * @class Enemy
+ */
 class Enemy{
+    /**
+     * @constructs Enemy
+     * @param items
+     */
     constructor(){
         this.sprite = 'images/enemy-bug.png';
         this.x = enemyXvalues[Math.floor(Math.random() * 3)];
@@ -20,8 +31,10 @@ class Enemy{
         this.speed = Math.floor(100 + (Math.random() * 200));
     }
 
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
+    /**
+     * @description updates the enemy's position
+     *  @param {number} dt - a time delta between ticks
+     */ 
     update(dt){
         this.x += this.speed * dt;
         // if enemy moves out of the canvas, following code changes enemy's setup
@@ -32,18 +45,23 @@ class Enemy{
         }
     }
 
-    // Draw the enemy on the screen, required method for game
+    /**
+    * @description  draws the enemy on the screen
+    */ 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
 }
 
-let gameWinned = false;
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-class Player{
 
+
+/**
+ * @class Player
+ */
+class Player{
+    /**
+     * @constructs Player
+     */
     constructor(){
         this.sprite = 'images/char-boy.png';
         this.x = 200;
@@ -51,6 +69,9 @@ class Player{
         this.lives = 0;
     }
 
+    /**
+    * @description updates the Player location 
+    */ 
     update(){
         if (this.y < 60) {
            if(!gameWinned){
@@ -68,10 +89,17 @@ class Player{
         }
     }
 
+    /**
+    * @description  draws the enemy on the screen
+    */ 
     render(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    /**
+    * @description moves the player according to that user input
+    * @param {string} input - the key which was pressed
+    */ 
     handleInput(input) {
         switch (input) {
             case 'left':
@@ -98,8 +126,9 @@ class Player{
         this.render();
     }
 
-    // check for collision between enemy and player
-    // limited number of collisions allowed, else Game over
+    /**
+    * @description  checks for collision between enemy and player
+    */ 
     checkCollision() { 
         for(let currentEnemy of allEnemies) {
 
@@ -116,6 +145,9 @@ class Player{
         }
     }
 
+    /**
+    * @description resets the player in the original position
+    */    
     reset() {
         this.x = 200;
         this.y = 400;
@@ -128,16 +160,20 @@ class Player{
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const totalEnemies = 3;
-let allEnemies = [];
-for (var i = 0; i < totalEnemies; i++) {
-    allEnemies.push(new Enemy());
+
+function startGame(){
+    for (var i = 0; i < totalEnemies; i++) {
+        allEnemies.push(new Enemy());
+    }
+    player = new Player();
 }
 
-var player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+/**
+* @description this listens for key presses and sends the keys to your
+* Player.handleInput() method. 
+*/ 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
